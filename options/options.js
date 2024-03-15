eeb7389d13d27cfce2f0d64835d5c9398eb0d49b
Crowdbotics <team@crowdbotics.com>
2024-03-15 16:25:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ai-prompt-catalogue-47090",
+  url: "https://ai-prompt-catalogue-47090.botics.co",
+  api: "https://ai-prompt-catalogue-47090.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
